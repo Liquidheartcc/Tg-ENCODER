7be65228ddd9c3aa5cd965cd60bd87c65492f63d
Hotmoon <aravind1555.av@gmail.com>
2024-05-01 18:46:59 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,3 @@
-# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
-
-import logging
 import asyncio
 import glob
 import inspect
@@ -18,38 +15,20 @@ import time
 import traceback
 from datetime import datetime as dt
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
-from logging.handlers import RotatingFileHandler
 from pathlib import Path
+
 import aiohttp
 import psutil
+import requests
 from telethon import Button, TelegramClient, errors, events, functions, types
 from telethon.sessions import StringSession
 from telethon.utils import pack_bot_file_id
+
 from .config import *
-LOG_FILE_NAME = "TGVid-Comp@Log.txt"
 
+basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
+LOGS = getLogger(__name__)
 
-
-if os.path.exists(LOG_FILE_NAME):
-    with open(LOG_FILE_NAME, "r+") as f_d:
-        f_d.truncate(0)
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=2097152000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
-)
-logging.getLogger("FastTelethon").setLevel(logging.INFO)
-logging.getLogger("urllib3").setLevel(logging.INFO)
-LOGS = logging.getLogger(__name__)
 
 try:
     bot = TelegramClient(None, APP_ID, API_HASH)
@@ -59,9 +38,10 @@ except Exception as e:
     LOGS.info(str(e))
     exit()
 
+
 async def startup():
     for x in OWNER.split():
         try:
-            await bot.send_message(int(x), "**Bot is Running 💨**")
+            await bot.send_message(int(x), "**Bot is Successfully Deployed**")
         except BaseException:
             pass
